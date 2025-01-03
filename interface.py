43d71085fefa6f5f8713b1a0cfcba4a756b30d3b
sstanlej <sstanlejj@gmail.com>
2025-01-03 17:12:34 +0100

    Started working on print_main_menu and print_list_of_teachers functions

--- interface.py
+++ interface.py
@@ -4,12 +4,14 @@ from classes import (Course, TeacherPlan,
 
 from datetime import time
 import pytest
-from data_lists import allcourselist
+from data_lists import allcourselist, allteacherlist
 import os
 
 
 def print_list_of_teachers():
-    pass
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('============================================================')
+    print('The list of all teachers:\n')
 
 
 def print_main_menu():
@@ -19,8 +21,9 @@ def print_main_menu():
     print(' that will help you plan courses for multiple groups.\n')
     print('What do you want to do?\n')
     print('1. See the list of all teachers and their plans')
+    print('============================================================')
     choice = input()
-    if choice == 1:
+    if choice == "1":
         print_list_of_teachers()
 
 
