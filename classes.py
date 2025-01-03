c737b6d7d2ee16eab8521753b03e8eb96d923c2e
sstanlej <sstanlejj@gmail.com>
2025-01-03 17:44:58 +0100

    Created Database class to store the lists of all teachers and courses instead of seprate global lists, removed data_lists.py file

--- classes.py
+++ classes.py
@@ -1,6 +1,3 @@
-from data_lists import allcourselist, allteacherlist
-
-
 class RoomCollisionError(Exception):
     pass
 
@@ -11,6 +8,63 @@ class GroupCollisionError(Exception):
 
 class WrongCourseIdError(Exception):
     pass
+
+
+class Database():
+    def __init__(self, allcourseslist, allteacherslist):
+        if not allcourseslist:
+            allcourseslist = []
+        self._allcourseslist = allcourseslist
+        if not allteacherslist:
+            allteacherslist = []
+        self._allteacherslist = allteacherslist
+
+    def add_teacher(self, newteacher):
+        self._allteacherslist.append(newteacher)
+
+    def add_course(self, newcourse):
+        self._allcourseslist.append(newcourse)
+
+    def remove_course(self, oldcourse):
+        self._allcourseslist.remove(oldcourse)
+
+    def get_allteacherslist(self):
+        return self._allteacherslist
+
+    def check_room_availability(self, nroom, nday, nstart_time, nfinish_time):
+        for course in self._allcourseslist:
+            day = course.get_day()
+            start_time = course.get_start_time()
+            finish_time = course.get_finish_time()
+            room = course.get_room()
+            if nroom == room:
+                if nday != day:
+                    pass
+                elif nstart_time > finish_time:
+                    pass
+                elif nfinish_time < start_time:
+                    pass
+                else:
+                    return False
+        return True
+
+    def check_group_availability(self, ngroup, nday,
+                                 nstart_time, nfinish_time):
+        for course in self._allcourseslist:
+            day = course.get_day()
+            start_time = course.get_start_time()
+            finish_time = course.get_finish_time()
+            group = course.get_group()
+            if ngroup == group:
+                if nday != day:
+                    pass
+                elif nstart_time > finish_time:
+                    pass
+                elif nfinish_time < start_time:
+                    pass
+                else:
+                    return False
+        return True
 
 
 class Course:
@@ -55,7 +109,6 @@ class TeacherPlan:
         if not courselist:
             self._courselist = []
         self._courselist = courselist
-        allteacherlist.append(self)
 
     def get_courselist(self):
         return self._courselist
@@ -69,68 +122,26 @@ class TeacherPlan:
     def get_name(self):
         return self._name
 
-    def update_allcourselist(self):
-        for course in allcourselist:
-            if course in self._courselist:
-                allcourselist.remove(course)
-        for course in self._courselist:
-            if course not in allcourselist:
-                allcourselist.append(course)
-
-    def check_room_availability(self, nroom, nday, nstart_time, nfinish_time):
-        for course in allcourselist:
-            day = course.get_day()
-            start_time = course.get_start_time()
-            finish_time = course.get_finish_time()
-            room = course.get_room()
-            if nroom == room:
-                if nday != day:
-                    pass
-                elif nstart_time > finish_time:
-                    pass
-                elif nfinish_time < start_time:
-                    pass
-                else:
-                    return False
-        return True
-
-    def check_group_availability(self, ngroup, nday,
-                                 nstart_time, nfinish_time):
-        for course in allcourselist:
-            day = course.get_day()
-            start_time = course.get_start_time()
-            finish_time = course.get_finish_time()
-            group = course.get_group()
-            if ngroup == group:
-                if nday != day:
-                    pass
-                elif nstart_time > finish_time:
-                    pass
-                elif nfinish_time < start_time:
-                    pass
-                else:
-                    return False
-        return True
-
-    def add_course(self, newcourse):
+    def add_course(self, newcourse, database):
         nday = newcourse.get_day()
         nstart_time = newcourse.get_start_time()
         nfinish_time = newcourse.get_finish_time()
         nroom = newcourse.get_room()
         ngroup = newcourse.get_group()
-        if not self.check_room_availability(nroom, nday,
-                                            nstart_time, nfinish_time):
+
+        if not database.check_room_availability(nroom, nday,
+                                                nstart_time, nfinish_time):
             raise RoomCollisionError("Room is not available at the time")
-        if not self.check_group_availability(ngroup, nday,
-                                             nstart_time, nfinish_time):
+        if not database.check_group_availability(ngroup, nday,
+                                                 nstart_time, nfinish_time):
             raise GroupCollisionError("Group is not availavle at the time")
         self._courselist.append(newcourse)
-        allcourselist.append(newcourse)
+        database.add_course(newcourse)
 
-    def remove_course(self, courseid):
+    def remove_course(self, courseid, database):
         for course in self._courselist:
             if course.get_id() == courseid:
                 self._courselist.remove(course)
-                allcourselist.remove(course)
+                database.remove_course(course)
                 return 0
         raise WrongCourseIdError("There is no course with specified ID")
--- test_classes.py
+++ test_classes.py
@@ -1,10 +1,9 @@
-from classes import (Course, TeacherPlan,
+from classes import (Course, TeacherPlan, Database,
                      GroupCollisionError, RoomCollisionError,
                      WrongCourseIdError)
 
 from datetime import time
 import pytest
-from data_lists import allcourselist, allteacherlist
 
 
 def test_create_course():
@@ -27,49 +26,56 @@ def test_create_teacherplan():
     start_time2 = time(10, 15)
     finish_time2 = time(12, 0)
 
+    database1 = Database([], [])
+
     course1 = Course(0, 'mako', 104, 's13',
                      start_time1, finish_time1, 'wednesday')
     course2 = Course(1, 'mako', 104, 's13',
                      start_time2, finish_time2, 'wednesday')
+
     teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [course1, course2])
-    teacherplan1.update_allcourselist()
+    database1.add_teacher(teacherplan1)
+
     assert teacherplan1.get_name() == 'Jan'
     assert teacherplan1.get_surname() == 'Ban'
     courselist = teacherplan1.get_courselist()
     assert courselist[0].get_name() == 'mako'
     assert courselist[1].get_finish_time() == finish_time2
-    assert len(allteacherlist) == 1
+    assert len(database1.get_allteacherslist()) == 1
 
 
 def test_add_course():
-    allcourselist.clear()
-    allteacherlist.clear()
     start_time1 = time(8, 15)
     finish_time1 = time(10, 0)
     start_time2 = time(10, 15)
     finish_time2 = time(12, 0)
+
+    database1 = Database([], [])
 
     course1 = Course(0, 'mako', 104, 's13',
                      start_time1, finish_time1, 'wednesday')
     course2 = Course(1, 'mako', 104, 's13',
                      start_time2, finish_time2, 'wednesday')
+
     teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [])
-    teacherplan1.add_course(course1)
+    database1.add_teacher(teacherplan1)
+
+    teacherplan1.add_course(course1, database1)
     courselist = teacherplan1.get_courselist()
     assert courselist[0].get_name() == 'mako'
-    teacherplan1.add_course(course2)
+    teacherplan1.add_course(course2, database1)
     courselist = teacherplan1.get_courselist()
     assert courselist[1].get_finish_time() == finish_time2
-    assert len(allteacherlist) == 1
+    assert len(database1.get_allteacherslist()) == 1
 
 
 def test_remove_course():
-    allcourselist.clear()
-    allteacherlist.clear()
     start_time1 = time(8, 15)
     finish_time1 = time(10, 0)
     start_time2 = time(10, 15)
     finish_time2 = time(12, 0)
+
+    database1 = Database([], [])
 
     course0 = Course(0, 'mako', 104, 's13',
                      start_time1, finish_time1, 'wednesday')
@@ -77,27 +83,28 @@ def test_remove_course():
                      start_time2, finish_time2, 'wednesday')
 
     teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [])
-    teacherplan1.update_allcourselist()
-    teacherplan1.add_course(course0)
-    teacherplan1.add_course(course1)
+    database1.add_teacher(teacherplan1)
+
+    teacherplan1.add_course(course0, database1)
+    teacherplan1.add_course(course1, database1)
     courselist = teacherplan1.get_courselist()
     assert len(courselist) == 2
-    teacherplan1.remove_course(0)
+    teacherplan1.remove_course(0, database1)
     assert len(courselist) == 1
     with pytest.raises(WrongCourseIdError):
-        teacherplan1.remove_course(0)
-    assert len(allteacherlist) == 1
+        teacherplan1.remove_course(0, database1)
+    assert len(database1.get_allteacherslist()) == 1
 
 
 def test_course_collision():
-    allcourselist.clear()
-    allteacherlist.clear()
     start_time1 = time(8, 15)
     finish_time1 = time(10, 0)
     start_time2 = time(9, 15)
     finish_time2 = time(11, 0)
     start_time3 = time(10, 0)
     finish_time3 = time(11, 45)
+
+    database1 = Database([], [])
 
     course0 = Course(0, 'mako', 104, 's13',
                      start_time1, finish_time1, 'wednesday')
@@ -110,16 +117,16 @@ def test_course_collision():
 
     teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [])
     teacherplan2 = TeacherPlan('San', 'Tan', [108], [])
-    teacherplan1.update_allcourselist()
-    teacherplan2.update_allcourselist()
+    database1.add_teacher(teacherplan1)
+    database1.add_teacher(teacherplan2)
 
-    teacherplan1.add_course(course0)
+    teacherplan1.add_course(course0, database1)
     courselist = teacherplan1.get_courselist()
     assert courselist[0].get_name() == 'mako'
     with pytest.raises(GroupCollisionError):
-        teacherplan1.add_course(course1)
+        teacherplan1.add_course(course1, database1)
     with pytest.raises(RoomCollisionError):
-        teacherplan2.add_course(course2)
+        teacherplan2.add_course(course2, database1)
     with pytest.raises(RoomCollisionError):
-        teacherplan2.add_course(course3)
-    assert len(allteacherlist) == 2
+        teacherplan2.add_course(course3, database1)
+    assert len(database1.get_allteacherslist()) == 2
