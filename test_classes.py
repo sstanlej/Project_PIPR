53739bf90836f434f3a25e413da9838f740c84de
sstanlej <sstanlejj@gmail.com>
2025-01-03 12:36:57 +0100

    Added test_create_teacherplan function

--- test_classes.py
+++ test_classes.py
@@ -6,7 +6,7 @@ from datetime import time
 
 
 def test_create_course():
-    start_time = time(8, 0)
+    start_time = time(8, 15)
     finish_time = time(10, 0)
     course1 = Course('mako', 104, 's13', start_time, finish_time, 'wednesday')
     assert course1.get_name() == 'mako'
@@ -15,3 +15,20 @@ def test_create_course():
     assert course1.get_start_time() == start_time
     assert course1.get_finish_time() == finish_time
     assert course1.get_day() == 'wednesday'
+
+
+def test_create_teacherplan():
+    start_time1 = time(8, 15)
+    finish_time1 = time(10, 0)
+    course1 = Course('mako', 104, 's13',
+                     start_time1, finish_time1, 'wednesday')
+    start_time2 = time(10, 15)
+    finish_time2 = time(12, 0)
+    course2 = Course('mako', 104, 's13',
+                     start_time2, finish_time2, 'wednesday')
+    teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [course1, course2])
+    assert teacherplan1.get_name() == 'Jan'
+    assert teacherplan1.get_surname() == 'Ban'
+    courselist = teacherplan1.get_courselist()
+    assert courselist[0].get_name() == 'mako'
+    assert courselist[1].get_finish_time() == finish_time2
