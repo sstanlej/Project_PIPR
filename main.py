8bd78e6b825f32aa838ce8a63989f54a47d6a88b
sstanlej <sstanlejj@gmail.com>
2025-01-03 18:26:01 +0100

    Main menu is now printed at the start of the program

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from interface import (print_main_menu,
-                       print_list_of_teachers)
+                       print_list_of_teachers,
+                       clear)
 
 
 def main():
-    print('hi')
+    clear()
+    print_main_menu()
 
 
 if __name__ == '__main__':
