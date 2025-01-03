a471dd2bd32214fee4f90c04cd06b320e3ee6d40
sstanlej <sstanlejj@gmail.com>
2025-01-03 16:12:21 +0100

    Added test_remove_course function

--- test_classes.py
+++ test_classes.py
@@ -1,14 +1,17 @@
 from classes import (Course, TeacherPlan,
-                     GroupCollisionError, RoomCollisionError)
+                     GroupCollisionError, RoomCollisionError,
+                     WrongCourseIdError)
 
 from datetime import time
 import pytest
+from all_course_list import allcourselist
 
 
 def test_create_course():
     start_time = time(8, 15)
     finish_time = time(10, 0)
-    course1 = Course(0, 'mako', 104, 's13', start_time, finish_time, 'wednesday')
+    course1 = Course(0, 'mako', 104, 's13',
+                     start_time, finish_time, 'wednesday')
     assert course1.get_id() == 0
     assert course1.get_name() == 'mako'
     assert course1.get_group() == 104
@@ -51,6 +54,28 @@ def test_add_course():
     teacherplan1.add_course(course2)
     courselist = teacherplan1.get_courselist()
     assert courselist[1].get_finish_time() == finish_time2
+
+
+def test_remove_course():
+    start_time1 = time(8, 15)
+    finish_time1 = time(10, 0)
+    start_time2 = time(10, 15)
+    finish_time2 = time(12, 0)
+
+    course0 = Course(0, 'mako', 104, 's13',
+                     start_time1, finish_time1, 'wednesday')
+    course1 = Course(1, 'mako', 104, 's13',
+                     start_time2, finish_time2, 'wednesday')
+
+    teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [])
+    teacherplan1.add_course(course0)
+    teacherplan1.add_course(course1)
+    courselist = teacherplan1.get_courselist()
+    assert len(courselist) == 2
+    teacherplan1.remove_course(0)
+    assert len(courselist) == 1
+    with pytest.raises(WrongCourseIdError):
+        teacherplan1.remove_course(0)
 
 
 def test_course_collision():
