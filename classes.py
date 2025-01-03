9e99b4d2689fb0f75fb8346d6355731bd3b25000
sstanlej <sstanlejj@gmail.com>
2025-01-03 11:22:52 +0100

    Created add_course function, added two basic conditions to TeacherPlan constructor and created allcourselist containing all courses of all teachers

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+from all_course_list import allcourselist
+
+
 class Course:
     def __init__(self, name, group, room, start_time, finish_time, day):
         self._name = name
@@ -12,5 +15,12 @@ class TeacherPlan:
     def __init__(self, name, surname, grouplist, courselist):
         self._name = name
         self._surname = surname
+        if not grouplist:
+            self._grouplist = []
         self._grouplist = grouplist
+        if not courselist:
+            self._courselist = []
         self._courselist = courselist
+
+    def add_course(self, course):
+        self._courselist.append(course)
