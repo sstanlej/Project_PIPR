a2911049c8c866695e8743d10bf0e2b6aebcab1b
sstanlej <sstanlejj@gmail.com>
2025-01-03 18:25:26 +0100

    Added clear function and option to exit the program

--- interface.py
+++ interface.py
@@ -1,14 +1,18 @@
 import os
 
 
-def print_list_of_teachers():
+def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def print_list_of_teachers():
+    clear()
     print('============================================================')
     print('The list of all teachers:\n')
 
 
 def print_main_menu():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear()
     print('============================================================')
     print('Welcome to Course Planner app,')
     print(' that will help you plan courses for multiple groups.\n')
@@ -18,7 +22,10 @@ def print_main_menu():
     print('3. Log in as a teacher')
     print('4. Save current data into a file')
     print('5. Load data from a file')
+    print('0. Exit')
     print('============================================================')
     choice = input(">> ")
     if choice == "1":
         print_list_of_teachers()
+    elif choice == "0":
+        return
