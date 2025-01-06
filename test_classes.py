35a18c5b301966ef3bc8ce47882c6d3a050d3406
sstanlej <sstanlejj@gmail.com>
2025-01-06 23:17:02 +0100

    Fixed tests

--- test_classes.py
+++ test_classes.py
@@ -1,6 +1,6 @@
 from classes import (Course, TeacherPlan, Database,
-                     GroupCollisionError, RoomCollisionError,
-                     WrongCourseIdError)
+                     RoomCollisionError, WrongCourseIdError,
+                     TeacherCollisionError)
 
 from datetime import time
 import pytest
@@ -28,12 +28,12 @@ def test_create_teacherplan():
 
     database1 = Database([], [])
 
-    course1 = Course(0, 'mako', 104, 's13',
+    course1 = Course('c1', 'mako', 104, 's13',
                      start_time1, finish_time1, 'wednesday')
-    course2 = Course(1, 'mako', 104, 's13',
+    course2 = Course('c2', 'mako', 104, 's13',
                      start_time2, finish_time2, 'wednesday')
 
-    teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [course1, course2])
+    teacherplan1 = TeacherPlan('t1', 'Jan', 'Ban', [course1, course2])
     database1.add_teacher(teacherplan1)
 
     assert teacherplan1.get_name() == 'Jan'
@@ -97,33 +97,27 @@ def test_remove_course():
 
 
 def test_course_collision():
-    start_time1 = time(8, 15)
-    finish_time1 = time(10, 0)
-    start_time2 = time(9, 15)
-    finish_time2 = time(11, 0)
-    start_time3 = time(10, 0)
-    finish_time3 = time(11, 45)
 
     database1 = Database([], [])
 
-    course0 = Course(0, 'mako', 104, 's13',
-                     start_time1, finish_time1, 'wednesday')
-    course1 = Course(1, 'mako', 104, 's14',
-                     start_time2, finish_time2, 'wednesday')
-    course2 = Course(2, 'anma', 108, 's13',
-                     start_time1, finish_time1, 'wednesday')
-    course3 = Course(3, 'anma', 107, 's13',
-                     start_time3, finish_time3, 'wednesday')
+    course0 = Course('c0', 'mako', 104, 's13',
+                     time(8, 15), time(10, 0), 'wednesday')
+    course1 = Course('c1', 'mako', 104, 's14',
+                     time(9, 15), time(9, 15), 'wednesday')
+    course2 = Course('c2', 'anma', 108, 's13',
+                     time(8, 15), time(10, 0), 'wednesday')
+    course3 = Course('c3', 'anma', 107, 's13',
+                     time(10, 0), time(11, 45), 'wednesday')
 
-    teacherplan1 = TeacherPlan('Jan', 'Ban', [104], [])
-    teacherplan2 = TeacherPlan('San', 'Tan', [108], [])
+    teacherplan1 = TeacherPlan('t1', 'Jan', 'Ban', [])
+    teacherplan2 = TeacherPlan('t2', 'San', 'Tan', [])
     database1.add_teacher(teacherplan1)
     database1.add_teacher(teacherplan2)
 
     teacherplan1.add_course(course0, database1)
     courselist = teacherplan1.get_courselist()
     assert courselist[0].get_name() == 'mako'
-    with pytest.raises(GroupCollisionError):
+    with pytest.raises(TeacherCollisionError):
         teacherplan1.add_course(course1, database1)
     with pytest.raises(RoomCollisionError):
         teacherplan2.add_course(course2, database1)
