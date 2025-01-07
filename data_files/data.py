13454b146ad4bff267a9f88161bc099d7c1e75d6
sstanlej <sstanlejj@gmail.com>
2025-01-07 19:24:06 +0100

    Send

--- data_files/data.py
+++ data_files/data.py
@@ -1,6 +1,4 @@
-from classes import Database, TeacherPlan
-
-teacher1 = TeacherPlan('Stan', 'Ban', [101, 102, 103, 105], [])
-teacher2 = TeacherPlan('John', 'Pork', [101, 102, 103, 104, 105, 108], [])
+from classes import Database, TeacherPlan, Course
+from datetime import time
 
 database0 = Database([], [])
