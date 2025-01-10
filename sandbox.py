312eab4c136c0a0cbf1bcdca6ae76145cc798076
sstanlej <sstanlejj@gmail.com>
2025-01-10 21:50:08 +0100

    Final version

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-from interface import printbox
-
-
-printbox(['The epic title',
-          'Row 2'], ['Hello im testing',
-                     'this new function',
-                     'i hope it works?'], 80)
-
-print(int('siema'))
